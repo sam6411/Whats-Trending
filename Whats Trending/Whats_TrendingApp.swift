688b92cdd63d67d003610f7164b9e7f4f60e2a6f
irvsam <irvsam001@myuct.ac.za>
2024-01-09 19:07:53 +1100

    Initial Commit

---
+++ Whats Trending/Whats_TrendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whats_TrendingApp.swift
+//  Whats Trending
+//
+//  Created by sammy on 2024/01/09.
+//
+
+import SwiftUI
+
+@main
+struct Whats_TrendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
